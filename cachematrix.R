d80f1e8b50da9c51610e6cc26812c8fdb11c86d2
zihaohan <zihaohansang@gmail.com>
2017-01-01 14:54:50 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -19,11 +19,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 ## step 2: firstly identify whether inverse be caculuated
 cacheSolve <- function(x, ...) {
-      m <- x$getinverse()
+      m <- x$getinverse()## the test first
       if(!is.null(m)) {
             message("getting cached data")
             return(m)
-      }
+      } ## if no result in local, then calculate
       data <- x$get()
       m <- solve(data, ...)
       x$setinverse(m)
